8564a29f70cd595fe1d96e9f5bb975701424285c
pablo1038 <pablogutierrezramos1038@gmail.com>
2021-05-17 19:17:41 +0200

    Versión final del codigo, cambiamos la clase entera Log a una más eficiente y comentamos la clase Main.

--- Log.java
+++ Log.java
@@ -1,70 +1,35 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+import java.io.File;
 import java.util.Date;
-
-public class Log {
-
-    private BufferedWriter buffered;
-    private String ruta;
-
-    public Log(String ruta) throws IOException {
-        this.ruta = ruta;
-        this.open(true);
+ public class Log {
+    private static String nombreArchivo = null;
+    File file;
+    FileWriter fw ;    
+    Date fecha;
+    public Log() {
+        fecha = new Date();
     }
-
-    public Log(String ruta, boolean reset) throws IOException {
-        this.ruta = ruta;
-        this.open(!reset);
-    }
-
-    private void open(boolean append) throws IOException {
-        this.buffered = new BufferedWriter(new FileWriter(this.ruta, append));
-    }
-
-    public void addLine(String line) throws IOException {
-
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY HH:mm:ss");
-        String formatoFecha = sdf.format(new Date());
-        this.open(true);
-        this.buffered.write("[" + formatoFecha + "] " + line + "\n");
-        this.close();
-    }
-
-    public String[] getLines() throws FileNotFoundException, IOException {
-
-        ArrayList<String> linesFile = new ArrayList<>();
-
-        BufferedReader br = new BufferedReader(new FileReader(this.ruta));
-
-        String line;
-        while ((line = br.readLine()) != null) {
-            linesFile.add(line);
+    /**
+     * @param linea
+     * @throws IOException
+     */
+    public static void logFile(String linea) throws IOException {
+        String fecha = new SimpleDateFormat("[dd MM yyyy HH mm ss]").format(new Date());
+        if (nombreArchivo == null) {
+            nombreArchivo = fecha;
+                
         }
+        try {
+            File file = new File(nombreArchivo+ "-log" + ".txt");
         
-        br.close();
-
-        String[] lines = new String[linesFile.size()];
-        
-        for (int i = 0; i < linesFile.size(); i++) {
-            lines[i] = linesFile.get(i);
+        FileWriter fw = new FileWriter(file, true);
+        fw.write(fecha + " " + linea + "\n");
+        fw.flush();
+        fw.close();
+        } catch (IOException ex) {
+            System.out.println(ex + "Directorio no encontrado");
         }
-        
-        return lines;
     }
-    
-    public void resetLog() throws IOException{
-        this.open(false);
-        this.close();
-    }
-    
-    private void close() throws IOException{
-        this.buffered.close();
-    }
-    
 }
--- Main.java
+++ Main.java
@@ -2,9 +2,17 @@ import java.util.Scanner;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+/**
+ * 
+ * @author Pablo Gutiérrez Ramos
+ *
+ */
+
 
 public class Main {
-
+	
+	private static Log log;
+	
 	private static Scanner sc = new Scanner(System.in);
 	// Dado para elegir turno
 	private static int dado;
@@ -25,22 +33,28 @@ public class Main {
 
 	private static int casilla;
 
-	private static boolean correcto;
-
-	public static void main(String[] args) {
-		turno();
+	/**
+	 * Creamos el metodo main que organiza el programa para jugar.
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		Log.logFile("Empieza el juego");
+		turno(); 
 		do {
-			String ficha = tirada % 2 == 0 ? "X" : "O";
+			String ficha = tirada % 2 == 0 ? "X" : "O"; //Elegimos entre si la ficha es X o O si el contador tirada es par o impar
 			imprimirTablero(tablero);
 			jugada(ficha, tablero);
 			gano = victoria(tablero);
 			if (gano) {
 				imprimirTablero(tablero);
-				System.out.println("Ganaste" + "! " + nombreJugador + " que mongolico");
+				System.out.println("Buena partida! ha ganado: " + nombreJugador);
+				Log.logFile("Ha ganado " + nombreJugador);
 			}
 			if (tirada == 8 && !gano) {
 				imprimirTablero(tablero);
-				System.out.println("Empate!");
+				System.out.println("Empate, ha estado reñido, echad la revancha!");
+				Log.logFile("Los jugadores han empatado.");
 				gano = true;
 			}
 
@@ -48,21 +62,30 @@ public class Main {
 		} while (!gano);
 
 	}
-
+	/**
+	 * Metodo que imprime el tablero vacio.
+	 * @param tablero
+	 */
 	public static void imprimirTablero(String[] tablero) {
 		System.out.println("    " + tablero[12] + "   " + tablero[13] + "   " + tablero[14]);
 		System.out.println(tablero[9] + " | " + tablero[0] + " | " + tablero[1] + " | " + tablero[2] + " | ");
 		System.out.println(tablero[10] + " | " + tablero[3] + " | " + tablero[4] + " | " + tablero[5] + " | ");
 		System.out.println(tablero[11] + " | " + tablero[6] + " | " + tablero[7] + " | " + tablero[8] + " | ");
 	}
-
-	public static void jugada(String fichaJugador, String[] tablero) {
-		boolean repetida = true;
-		correcto = true;
+	
+	/**
+	 * 
+	 * @param fichaJugador
+	 * @param tablero
+	 * @throws IOException
+	 * Metodo que permite elegir casilla donde poner la ficha a los jugadores
+	 */
+	public static void jugada(String fichaJugador, String[] tablero) throws IOException {
 		Scanner coordenada = new Scanner(System.in);
 		nombreJugador = tirada % 2 == 0 ? nombreJugador1 : nombreJugador2;
 		System.out.println("Selecciona donde quieres poner la ficha " + nombreJugador + ": ");
-		posicion = coordenada.nextLine().toUpperCase();
+		Log.logFile(nombreJugador + " elige donde poner la ficha");
+		posicion = coordenada.nextLine().toUpperCase();//Declaramos el scanner para introducir las coordenadas por pantalla.
 		switch (posicion) {
 		case "A1":
 			casilla = 0;
@@ -94,7 +117,8 @@ public class Main {
 		}
 		//Coprobamos que la casilla esta ocupada, y si lo está volvemos a poner ficha.
 		if (tablero[casilla] == "X" || tablero[casilla] == "O") {
-			System.out.println("No puedes poner una ficha ahi, vuelve a tirar.");
+			Log.logFile(nombreJugador + " se equivoco y vuelve elegir posición");
+			System.out.println("No puedes poner una ficha ahi, vuelve a elegir posición,por favor.");
 			tirada = tirada - 1;
 		}
 		//Si no ponemos la ficha de manera normal
@@ -103,7 +127,11 @@ public class Main {
 		}
 	}
 	
-	//Comprobamos que coincide 
+	/**
+	 * Comprobamos que coinciden las lineas tanto horizontales como verticales y las diagonales.
+	 * @param tablero
+	 * @return
+	 */
 	public static boolean victoria(String tablero[]) {
 		if (tablero[0].equals(tablero[1]) && tablero[0].equals(tablero[2]) && !tablero[0].equals("_")) {
 			return true;
@@ -126,28 +154,39 @@ public class Main {
 
 	}
 
-	public static void turno() {
+	/**
+	 * @throws IOException
+	 */
+	public static void turno() throws IOException {
 		boolean eleccion = true;
 		System.out.println("Introduce el nombre del primer jugador: ");
 		nombreJugador1 = sc.nextLine();
 		System.out.println("El primer jugador es : " + nombreJugador1 + " y juega con X");
+		Log.logFile("El primer jugador es " + nombreJugador1);
 		System.out.println("Introduce el nombre del segundo jugador: ");
 		nombreJugador2 = sc.nextLine();
 		System.out.println("El segundo jugador es : " + nombreJugador2 + " y juega con O");
+		Log.logFile("El segundo jugador es " + nombreJugador2);
+
 
 		while (eleccion) {
 			int dado1 = (int) (Math.random() * (6 - 1 + 1) + 1);
 			System.out.println(nombreJugador1 + " ha sacado un:" + dado1);
+			Log.logFile(nombreJugador1 + " ha sacado un:" + dado1);
 			int dado2 = (int) (Math.random() * (6 - 1 + 1) + 1);
 			System.out.println(nombreJugador2 + " ha sacado un:" + dado2);
+			Log.logFile(nombreJugador2 + " ha sacado un:" + dado2);
+
 
 			if (dado1 > dado2) {
 				System.out.println("Ha ganado: " + nombreJugador1 + " es tu turno!");
+				Log.logFile(nombreJugador1 + " ganó, es su turno.");
 				eleccion = false;
 			} else if (dado1 == dado2) {
 				System.out.println("Empate! tirad otra vez!");
 			} else {
 				System.out.println("Ha ganado: " + nombreJugador2 + " es tu turno!");
+				Log.logFile(nombreJugador2 + " ganó, es su turno.");
 				eleccion = false;
 			}
 		}
